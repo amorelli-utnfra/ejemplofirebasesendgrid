ea75601ba79a4c7dfc995a5f3b8f3379eaf040ec
Augusto Morelli <augustomorelli206@gmail.com>
2022-10-28 22:58:09 -0300

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  
+  firebaseConfig: {
+    apiKey: "AIzaSyDyA1ONEjGUohP-3yHdEng-LkbgBku7frc",
+    authDomain: "ejsendgrid.firebaseapp.com",
+    projectId: "ejsendgrid",
+    storageBucket: "ejsendgrid.appspot.com",
+    messagingSenderId: "654269863713",
+    appId: "1:654269863713:web:942e34e04339f63e739060"
+  }
+};
+
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
