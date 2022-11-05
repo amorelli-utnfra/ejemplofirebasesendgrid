6daeac3ef6ca241b7514807e96ce5f8995880646
amorelli-utnfra <a.morelli@sistemas-utnfra.com.ar>
2022-11-05 11:22:43 -0300

    remove obsolet function

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Injectable({
@@ -15,8 +14,7 @@ export class UsersService {
   users: Observable<User[]>;
   token: string;
 
-  constructor( private afs: AngularFirestore,
-    private http: HttpClient) {
+  constructor( private afs: AngularFirestore) {
     this.usersCollection = afs.collection<User>('users');
     this.users = this.usersCollection.valueChanges();
    }
@@ -41,44 +39,7 @@ export class UsersService {
 
   }
 
-  sendMail(userId: string) {
-    let body = {
-      personalizations: [
-        {
-          from: {
-            email: 'duenolacomanda@gmail.com',
-            name: 'Dueño'
-          },
-          to: [
-            {
-              email: 'augustomorelli206@gmail.com',
-              name: 'Janice Doe'
-            }
-          ],
-          bcc: []
-        }
-      ],
-      from: {
-        email: 'duenolacomanda@gmail.com',
-        name: 'Example Order Confirmation'
-      },
-      subject: 'Prueba',
-      content: [
-        {
-          type: 'text/html',
-          value: '<p>Hello from Twilio SendGrid!</p><p>Sending with the email service trusted by developers and marketers for <strong>time-savings</strong>, <strong>scalability</strong>, and <strong>delivery expertise</strong>.</p><p>%open-track%</p>'
-        }
-      ],
-      }
-
-      let headers : HttpHeaders = new HttpHeaders().set('Authorization', 'Bearer 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')
-
-    return this.http.post("https://api.sendgrid.com/v3/mail/send", body, {headers});
-  }
-
 
 
 }
 
-
-// "Bearer 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"
